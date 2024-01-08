93963b160c6f9b7fb382e3ae004c085c4844b805
RonyAbreu <ronyelison78@gmail.com>
2024-01-08 18:15:02 -0300

    Criação do projeto

---
+++ src/main/java/com/ronyelison/blogjogos/BlogJogosApplication.java
@@ -0,0 +1,13 @@
+package com.ronyelison.blogjogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronyelison/blogjogos/BlogJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronyelison.blogjogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
